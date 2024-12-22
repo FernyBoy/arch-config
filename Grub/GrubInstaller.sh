014ab8cad98754a2b792492ff8ebfa5f21589439
Fernando Borquez <fernandoborquez215@gmail.com>
2024-12-22 09:42:30 -0700

    Grub config fixed

--- Grub/GrubInstaller.sh
+++ Grub/GrubInstaller.sh
@@ -1,4 +1,6 @@
 tar -xf argon-grub-theme-3.2.2.tar.gz
 mkdir -p /home/$USER/Docs/grub-config
+cd ./argon-grub-theme-3.2.2
+./install.sh --install --resolution 1920x1080 --background Night --fontsize 16
+cd ../
 mv argon-grub-theme-3.2.2 /home/$USER/Docs/grub-config/
-
